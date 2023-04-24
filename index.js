3c151426c1d226d9c8d71d104d292b025206cbad
Albert <skytool@users.noreply.github.com>
2023-04-23 23:52:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`;
+}
+
+mondayWork = function (work = "go to the office"){
+    return `This Monday, I will ${work}.`}
+
+mondayWork();
+
+function wrapAdjective(outer = "||special||"){
+    return function inside(wrap = "*"){
+        return `You are ${outer}${wrap}${outer}!`;
+    }
+}
